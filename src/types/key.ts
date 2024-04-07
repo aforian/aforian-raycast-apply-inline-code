998c7438620889ce4758ed476eca668f42360794
Alex Ian <alexian853@gmail.com>
2024-04-07 17:44:47 +0800

    chore: lint

--- src/types/key.ts
+++ src/types/key.ts
@@ -1,4 +1,4 @@
-import { keyMap } from "../constants/keyMap";
+import { keyMap } from '../constants/keyMap';
 
 export type Key = keyof typeof keyMap;
-export type Modifier = "command" | "control" | "shift" | "option";
+export type Modifier = 'command' | 'control' | 'shift' | 'option';
